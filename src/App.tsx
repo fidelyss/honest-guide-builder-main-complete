44b5a0ae0020cbc0e32011483102175955ac1f1e
fidelyss <alissonfidelis147@gmail.com>
2026-01-07 20:34:58 -0300

    pre sell complete sitelinks

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import WhatIsSection from "./components/WhatIsSection"
+import ProductsSection from "./components/ProductsSection"
+import ProsConsSection from "./components/ProsConsSection"
+import TestimonialsSection from "./components/TestimonialsSection"
 
 const queryClient = new QueryClient();
 
@@ -17,6 +21,10 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/WhatIs" element={<WhatIsSection />} />
+          <Route path="/Products" element={<ProductsSection />} />
+          <Route path="/ProsCons" element={<ProsConsSection />} />
+          <Route path="/Testimonials" element={<TestimonialsSection />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
